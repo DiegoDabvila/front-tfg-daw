48117bb55bd10bf72d60a93edbcb869bbf30ca3c
Diego Davila <diego.davilafreitas@gmail.com>
2023-10-31 19:29:28 +0100

    app(login/register):delete open modal

--- cuevanaplus/src/app/services/modal-service.service.ts
+++ cuevanaplus/src/app/services/modal-service.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import {MatDialog} from "@angular/material/dialog";
-import {MovieDetailComponent} from "../components/movie-detail/movie-detail.component";
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +8,6 @@ export class ModalServiceService {
 
   constructor(private dialog: MatDialog) {}
 
-  openMovieDetailModal(): void {
-    this.dialog.open(MovieDetailComponent, {
-      width: '400px',
-    });
-  }
 
 
 }
